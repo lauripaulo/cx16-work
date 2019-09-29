faea4a46ee9a24d2c990ed9a589bdab37312be54
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2019-09-29 15:11:18 -0300

    add verbosity and register vars otimizations

--- hello/build.sh
+++ hello/build.sh
@@ -1,13 +1,13 @@
 
 # Compiler
-cc65 -O -t cx16 hello.c
+cc65 -Or -v -t cx16 hello.c
 
 # Assembler
-ca65 hello.s
-ca65 -t cx16 text.s
+ca65 -v -t cx16 hello.s
+ca65 -v -t cx16 text.s
 
 # Linker
-ld65 -o hello.prg -t cx16 -Ln hello.lbl -m hello.map hello.o text.o c64.lib
+ld65 -v -o hello.prg -t cx16 -Ln hello.lbl -m hello.map hello.o text.o c64.lib
 
 # Emulator
-x16emu -scale 2 -run -prg hello.prg &
+x16emu -run -prg hello.prg &
