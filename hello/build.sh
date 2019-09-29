258294b721a37d32f3a78d76f02191823650005f
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2019-09-28 21:48:24 -0300

    Basic hello world program

---
+++ hello/build.sh
@@ -0,0 +1,13 @@
+
+# Compiler
+cc65 -O -t cx16 hello.c
+
+# Assembler
+ca65 hello.s
+ca65 -t cx16 text.s
+
+# Linker
+ld65 -o hello -t cx16 hello.o text.o c64.lib
+
+# Emulator
+#./x16emu -prg hello.prg,1 &
