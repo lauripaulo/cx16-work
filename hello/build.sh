d52c1a95ab232da486865c8c9849294b9915882e
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2019-09-28 23:19:19 -0300

    add vice label and map files

--- hello/build.sh
+++ hello/build.sh
@@ -7,7 +7,7 @@ ca65 hello.s
 ca65 -t cx16 text.s
 
 # Linker
-ld65 -o hello.prg -t cx16 hello.o text.o c64.lib
+ld65 -o hello.prg -t cx16 -Ln hello.lbl -m hello.map hello.o text.o c64.lib
 
 # Emulator
 x16emu -prg hello.prg &
